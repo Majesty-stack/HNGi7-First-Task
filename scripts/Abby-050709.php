39cd1b7c6667bf516ee9ac13e0f7f205e3083029
Majesty-stack <60063709+Majesty-stack@users.noreply.github.com>
2020-06-05 14:21:46 +0100

    Update Abby-050709.php

--- scripts/Abby-050709.php
+++ scripts/Abby-050709.php
@@ -6,11 +6,11 @@ $language = "PHP";
 $email = "amusaabiodun88@gmail.com";
 
 
-function show_details(){ 
+function show_info(){ 
     global $intern, $hng_id, $language, $email;
-    return "Hello World, this is $intern with HNGi7 ID $hng_id using $language for stage 2 task";
+    return "Hello World, this is $name with HNGi7 ID $hng_id using $language for stage 2 task and $email";
 }
 
-echo show_details();
+echo show_info();
 ?>
 
